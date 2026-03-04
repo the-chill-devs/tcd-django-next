f936898418ca97454ddb06849cf37571a576ce50
codeschris <chrismlolwa42@outlook.com>
2026-03-04 21:16:35 +0300

    complete frontend

--- app_frontend/app/page.tsx
+++ app_frontend/app/page.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
-    <div>Welcome Home</div>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Welcome to tcd-django-next template</h1>
+      <p className="text-lg">Created by the TCD team to make it easier to start a new project</p>
+    </main>
   );
 }
